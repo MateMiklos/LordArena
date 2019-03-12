22514f78416a75b7ce02600c08837720c5b559a7
MateMiklos <miklos.mate.91@gmail.com>
2019-03-13 00:00:01 +0100

    database has been set

---
+++ data_handler.py
@@ -0,0 +1,18 @@
+import database_common
+
+
+@database_common.connection_handler
+def get_characters_name(cursor):
+    cursor.execute("""
+        SELECT character_name FROM characters;
+        """)
+    characters = cursor.fetchone()
+    return characters
+
+
+@database_common.connection_handler
+def create_character(cursor, character_name):
+    cursor.execute("""
+        INSERT INTO characters(character_name)
+        VALUES(%(character_name)s;
+        """, {'character_name': character_name})
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
+import data_handler
+
 
 app = Flask(__name__)
 
@@ -11,6 +13,12 @@ def route_index():
 @app.route('/game')
 def route_game():
     return render_template('game.html')
+
+
+@app.route('/character-create')
+def route_character_create():
+    characters = data_handler.get_characters_name()
+    return render_template('character-create.html', characters=characters)
 
 
 def main():
